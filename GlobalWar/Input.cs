d7411786b8e5b7c7a10162fd6daec61a10b204d1
Uljas Lindell <antero.lindell@gmail.com>
2022-11-25 03:01:22 +0200

    window handling and input system

--- GlobalWar/Input.cs
+++ GlobalWar/Input.cs
@@ -1,7 +1,33 @@
+using System;
+using Raylib_cs;
+
 namespace GlobalWar
 {
-    public interface Input
+    public interface IInput
     {
-        
+        public static void HandleCoreInput()
+        {
+            var keyPress = Raylib.GetCharPressed();
+
+            if (keyPress != 0)
+                Console.WriteLine(char.ConvertFromUtf32(keyPress));
+
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_F))
+                Raylib.ToggleFullscreen();
+        }
+
+        public static bool OnRollOverUI(Rectangle button)
+        {
+            return Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), button);
+        }
+
+        public static bool OnClickUI(Rectangle button)
+        {
+            if (Raylib.IsMouseButtonPressed(0) && OnRollOverUI(button))
+            {
+                return Raylib.IsMouseButtonReleased(0) && OnRollOverUI(button);
+            }
+            return false;
+        }
     }
 }
--- GlobalWar/Program.cs
+++ GlobalWar/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Numerics;
+using Raylib_cs;
+using static GlobalWar.IInput;
+using static Raylib_cs.Raylib;
 
 namespace GlobalWar
 {
-    class Program
+    namespace Raylib_cs.Raylib
     {
-        static void Main(string[] args)
+        static class Program
         {
-            Console.WriteLine("Hello World!");
+            private static Vector2 _screenRes = new Vector2(1280, 720);
+            private const string GameTitle = "Global Conflict: Armageddon";
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("args:" + args);
+                Console.WriteLine("64-bit application: " + Environment.Is64BitProcess);
+                Console.WriteLine("Platform: " + Environment.OSVersion);
+                Console.WriteLine("CPU core count: " + Environment.ProcessorCount);
+                Console.WriteLine("Initializing window with a resolution of " + _screenRes + "\n");
+
+                InitWindow((int)_screenRes.X, (int)_screenRes.Y, GameTitle);
+                SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+                while (!WindowShouldClose())
+                {
+                    HandleCoreInput();
+                    DrawGame();
+                }
+                
+                CloseWindow();
+            }
+
+            static void DrawGame()
+            {
+                BeginDrawing();
+                ClearBackground(Color.DARKGRAY);
+                DrawFPS((int)_screenRes.X / 32, (int)_screenRes.Y / 32);
+                EndDrawing();
+            }
         }
     }
 }
